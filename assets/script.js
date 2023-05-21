1dd0b8ce029338ef7382dd2d97bd1fce34739469
milkibeka <125259404+milkibeka@users.noreply.github.com>
2023-05-21 14:22:03 +0100

    past, present and future that styles are given in css are defined by comparing to currentHour that I console logged before

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,10 @@ var now = moment().format("dddd, MMMM Do");
     $("#currentDay").text(now);
     const currentHour = moment().format("HH");
     console.log(currentHour);
+    $(".time-div").each(function() {
+        const timeDiv = $(this).attr("id").split("-")[1];
+        $(this).toggleClass("past", currentHour > timeDiv);
+        $(this).toggleClass("present", currentHour == timeDiv);
+        $(this).toggleClass("future", currentHour < timeDiv);
+      });
+    
